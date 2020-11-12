b3c755f1f43a5ec2ff7ce42b5ec240b05e8e6ce5
sharoll <syuliannaaraujo@unicesar.edu.co>
2020-11-12 18:52:49 -0500

    first commit

---
+++ Entidad/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Entidad
+{
+    public class Persona
+    {
+        public string TipoDocumento {get;set;}
+        public string Identificacion {get;set}
+        public string Nombre {get;set;}
+        public string Direccion {get;set;}
+        public string Telefono {get;set;}
+        public string Pais{get;set}
+        public string Departamento {get;set;}
+        public string Ciudad {get;set;}
+    }
+}
